e13b63978dae44797dfa4d8a6914e99e90532316
siamCSEjnu <99203101+siamCSEJnU@users.noreply.github.com>
2023-04-30 20:41:08 +0600

    header component is done

---
+++ src/pages/shared/LeftNav/LeftNav.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LeftNav = () => {
+  return <div>this is left nav</div>;
+};
+
+export default LeftNav;
---
+++ src/pages/shared/RightNav/RightNav.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RightNav = () => {
+  return <div>this is right nav</div>;
+};
+
+export default RightNav;
